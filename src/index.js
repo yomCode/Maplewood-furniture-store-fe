ed88b83eb271531c5643c93bfb04999326f6731b
gsure-tech <Geeescode@>
2023-01-22 20:44:56 +0100

    Signup and Login backend connected

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ProductProvider } from './context/productContext';
+import DataProvider from './context/authcontext';
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ProductProvider>
-      <App />
+      <DataProvider>
+        <App />
+      </DataProvider>
     </ProductProvider>
   </React.StrictMode>
 );
--- src/screens/Login/FormLogin.jsx
+++ src/screens/Login/FormLogin.jsx
@@ -1,46 +1,66 @@
 import "./Login.css";
-import React from 'react'
+import React, { useState } from 'react';
+import { useAuth } from "../../context/authcontext";
 
 const Login = () => {
 
+  const { LoginConfig } = useAuth();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    LoginConfig(formData);
+    setFormData({
+      email: "",
+      password: "",
+    });
+  };
 
 
   return (
     <div className="login_bg_image">
-      
-       
-            <form
-              // id="form"
-              className="loginForm "
-            >
-              <h2 className="login_h2">Login</h2>
+      <form
+        // id="form"
+        className="loginForm "
+        onSubmit={handleSubmit}
+      >
+        <h2 className="login_h2">Login</h2>
 
-              <input
-                type="email"
-                name="email"
-                //  {...register("email")}
-                placeholder="Email"
-                required
-              />
+        <input
+          type="email"
+          name="email"
+          //  {...register("email")}
+          placeholder="Email"
+          required
+          onChange={handleChange}
+        />
 
-              <input
-                type="password"
-                name="password"
-                placeholder="password"
-                required
-              />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          required
+          onChange={handleChange}
+        />
 
-              <button className="login_btn">Login</button>
-              <p className="login_small mb-0">
-                <a href="/forgotpassword">Forgotten password?</a>
-              </p>
+        <button className="login_btn" type="submit">Login</button>
+        <p className="login_small mb-0">
+          <a href="/forgotpassword">Forgotten password?</a>
+        </p>
 
-              <p className="login_small mb-0">
-                Don't have an account ?<a href="/signup">Sign up</a>
-              </p>
-            </form>
-          
-        </div>
+        <p className="login_small mb-0">
+          Don't have an account ?<a href="/signup">Sign up</a>
+        </p>
+      </form>
+    </div>
   );
 }
 
