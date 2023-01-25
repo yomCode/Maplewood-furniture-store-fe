742ae7fe223d110a8b3935dab751021c6e2d1557
Abayomi Mustapha <107572498+yomCode@users.noreply.github.com>
2023-01-25 08:43:38 +0100

    update

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -4,12 +4,14 @@ import { AiOutlineInbox, AiOutlineHeart, AiOutlineCloseCircle } from "react-icon
 import { BiEditAlt } from "react-icons/bi";
 import SideBarElement from '../SideBarElement/SideBarElement';
 import "./sideBar.css";
+import { useAuth } from '../../context/authcontext';
 
 const SideBar = () => {
+  const { Logout } = useAuth();
   return (
     <div className='bg-white drop-shadow-md rounded-md lg:h-100'>
       <div>
-        <SideBarElement name="My Oakland Account" icon={<FaRegUser to="/"/>}/>
+        <SideBarElement name="My Oakland Account" icon={<FaRegUser />} to="/dashboard" />
         <SideBarElement name="Orders" icon={<AiOutlineInbox to="/contactus"/>}/>
         <SideBarElement name="Favourites" icon={<AiOutlineHeart to="/"/>}/>
         <SideBarElement name="Edit Profile" icon={<BiEditAlt />} to="/dashboard-acc-info"/>
@@ -18,7 +20,7 @@ const SideBar = () => {
       </div>
       
         <div className='flex justify-center border-t-2 border-gray-200 p-3 mt-2 w-100'>
-          <button className='text-orange-200 text-xl font-bold-900'>LOGOUT</button>
+          <button onClick={() => Logout()} className='text-orange-500 text-xl font-bold-900'>LOGOUT</button>
         </div>
     </div>
   )
