146d988fff5f2621b0cee801fcb1468d172ac3e8
Abayomi Mustapha <mustaphaabayomi71@gmail.com>
2023-06-26 11:00:48 +0100

    Dummy products added

--- src/components/BestSelling.js
+++ src/components/BestSelling.js
@@ -11,6 +11,30 @@ const BestSelling = () => {
     // eslint-disable-next-line
   }, []);
 
+  const dummyProducts = [
+    {
+      id: 1,
+      name: "LAMP",
+      imageUrl:
+        "https://templatekit.jegtheme.com/funiture/wp-content/uploads/sites/18/2020/11/NAM2CS@2x-300x300.jpg",
+      price: "100,000",
+    },
+    {
+      id: 2,
+      name: "MODERN",
+      imageUrl:
+        "https://templatekit.jegtheme.com/funiture/wp-content/uploads/sites/18/2020/11/Group-1@2x-300x300.jpg",
+      price: "100,000",
+    },
+    {
+      id: 3,
+      name: "DRESSER",
+      imageUrl:
+        "https://templatekit.jegtheme.com/funiture/wp-content/uploads/sites/18/2020/11/YMN7ZV@2x-300x300.jpg",
+      price: "100,000",
+    },
+  ];
+
   return (
     <div className="">
       <h6
@@ -25,6 +49,16 @@ const BestSelling = () => {
       <div className="mt-8 flex justify-center flex-wrap gap-5">
         {bestSelling.length > 0 ? (
           bestSelling.map((product) => (
+            <BestSellingCard
+              pName={product.name}
+              price={product.price}
+              source={product.imageUrl}
+              alt="#"
+              id={product.id}
+            />
+          ))
+        ) : dummyProducts?.length > 0 ? (
+          dummyProducts.map((product) => (
             <BestSellingCard
               pName={product.name}
               price={product.price}
--- src/components/ProductOverview.js
+++ src/components/ProductOverview.js
@@ -11,11 +11,41 @@ const ProductOverview = () => {
     // eslint-disable-next-line
   }, []);
 
+  const dummyProducts = [
+    {
+      id: 1,
+      name: "WOODEN",
+      imageUrl:
+        "https://templatekit.jegtheme.com/funiture/wp-content/uploads/sites/18/2020/11/92DNEPD@2x-300x300.jpg",
+    },
+    {
+      id: 2,
+      name: "SOFA",
+      imageUrl:
+        "https://templatekit.jegtheme.com/funiture/wp-content/uploads/sites/18/2020/11/4PHLD2@2x-300x300.jpg",
+    },
+    {
+      id: 3,
+      name: "CUPBOARD",
+      imageUrl:
+        "https://th.bing.com/th/id/R.283fb4960158a3608b8b2fbb553d10ab?rik=74UYA0mjX3VG1g&pid=ImgRaw&r=0",
+    },
+  ];
+
   return (
     <div className="max-w-[90%] mt-[-30px] p-4 bg-white mx-auto">
       <div className="flex flex-wrap gap-[2rem] justify-center">
         {newArrival.length > 0 ? (
           newArrival.map((product) => (
+            <Card
+              title="NEW ARRIVAL"
+              pName={product.name}
+              image={product.imageUrl}
+              id={product.id}
+            />
+          ))
+        ) : dummyProducts?.length > 0 ? (
+          dummyProducts.map((product) => (
             <Card
               title="NEW ARRIVAL"
               pName={product.name}
